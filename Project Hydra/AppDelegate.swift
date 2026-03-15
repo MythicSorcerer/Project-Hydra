37f585a139fb1d0ffb0ce19444b78ab55d9599b0
Mythic <ximo.tu@icloud.com>
2026-03-14 23:35:09 -0700

    Initial Commit

---
+++ Project Hydra/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Project Hydra
+//
+//  Created by Ximo Tu on 3/14/26.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
